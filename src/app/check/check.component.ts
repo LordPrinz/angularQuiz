427c385e2f0641ef84ea978e1658a7fe9f021af7
LordPrinz <plazinski2902@gmail.com>
2023-03-11 18:30:23 +0100

    Create reset method

--- src/app/check/check.component.ts
+++ src/app/check/check.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { optionType } from 'types/optionType';
 import { StateService } from '../state.service';
 
 @Component({
@@ -9,14 +10,28 @@ import { StateService } from '../state.service';
 export class CheckComponent implements OnInit {
   public isAllSelected: boolean = false;
 
+  public stateTemp: optionType[] | null = null;
+
   activateCheckMode() {
     this.state.activateMode();
+  }
+
+  reset() {
+    this.state.deactivateMode();
+
+    const resetedState = this.stateTemp!.map((state) => {
+      state.selectedTo = null;
+      return state;
+    });
+
+    this.state.setState(resetedState);
   }
 
   constructor(private state: StateService) {}
   ngOnInit(): void {
     this.state.state$.subscribe((newState) => {
       this.isAllSelected = !newState.find((item) => !item.selectedTo);
+      this.stateTemp = newState;
     });
   }
 }
