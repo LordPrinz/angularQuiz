67971a9efb5a3211b58d61594e4f381ec2e3adef
LordPrinz <plazinski2902@gmail.com>
2023-03-11 17:31:07 +0100

    Configure plant box

--- src/app/plants/plants.component.ts
+++ src/app/plants/plants.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { optionType } from 'types/optionType';
+import { StateService } from '../state.service';
 
 @Component({
   selector: 'app-plants',
   templateUrl: './plants.component.html',
-  styleUrls: ['./plants.component.css']
+  styleUrls: ['./plants.component.css'],
 })
 export class PlantsComponent implements OnInit {
+  public plants: optionType[] | null = null;
 
-  constructor() { }
+  constructor(private state: StateService) {}
 
   ngOnInit(): void {
+    this.state.state$.subscribe((newState) => {
+      this.plants = newState.filter((thing) => thing.selectedTo == 'plant');
+    });
   }
-
 }
