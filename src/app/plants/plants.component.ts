96795cab891601cb4690828b6add18bd6bd8cfce
LordPrinz <plazinski2902@gmail.com>
2023-03-11 18:38:12 +0100

    Implement Check plants logic

--- src/app/plants/plants.component.ts
+++ src/app/plants/plants.component.ts
@@ -9,12 +9,16 @@ import { StateService } from '../state.service';
 })
 export class PlantsComponent implements OnInit {
   public plants: optionType[] | null = null;
+  public isCheckmode: boolean = false;
 
   constructor(private state: StateService) {}
 
   ngOnInit(): void {
     this.state.state$.subscribe((newState) => {
       this.plants = newState.filter((thing) => thing.selectedTo == 'plant');
+    });
+    this.state.modeState$.subscribe((newState) => {
+      this.isCheckmode = newState;
     });
   }
 }
