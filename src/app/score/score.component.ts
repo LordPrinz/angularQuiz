7c87a0862ddc9044c96af8c4d4436a03ef5510a1
LordPrinz <plazinski2902@gmail.com>
2023-03-11 19:05:37 +0100

    Bind checkmode to counter

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from '../state.service';
 
 @Component({
   selector: 'app-score',
   templateUrl: './score.component.html',
-  styleUrls: ['./score.component.css']
+  styleUrls: ['./score.component.css'],
 })
 export class ScoreComponent implements OnInit {
+  public isCheckMode: boolean = false;
+  public counter: number = 0;
 
-  constructor() { }
+  constructor(private state: StateService) {}
 
   ngOnInit(): void {
+    this.state.modeState$.subscribe((state) => (this.isCheckMode = state));
   }
-
 }
