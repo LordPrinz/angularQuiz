b2d53135394a0f9ef701e4d452b2badc7441402c
LordPrinz <plazinski2902@gmail.com>
2023-03-11 18:12:15 +0100

    Add check for no options logic

--- src/app/check/check.component.ts
+++ src/app/check/check.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from '../state.service';
 
 @Component({
   selector: 'app-check',
   templateUrl: './check.component.html',
-  styleUrls: ['./check.component.css']
+  styleUrls: ['./check.component.css'],
 })
 export class CheckComponent implements OnInit {
+  public isAllSelected: boolean = false;
 
-  constructor() { }
-
+  constructor(private state: StateService) {}
   ngOnInit(): void {
+    this.state.state$.subscribe((newState) => {
+      this.isAllSelected = !newState.find((item) => !item.selectedTo);
+    });
   }
-
 }
