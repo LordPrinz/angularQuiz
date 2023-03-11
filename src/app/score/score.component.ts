056d5baf914d8f389f5e1c26c798345abae54614
LordPrinz <plazinski2902@gmail.com>
2023-03-11 19:11:25 +0100

    Implement counter logic

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { optionType } from 'types/optionType';
 import { StateService } from '../state.service';
 
 @Component({
@@ -10,9 +11,24 @@ export class ScoreComponent implements OnInit {
   public isCheckMode: boolean = false;
   public counter: number = 0;
 
+  public currentState: optionType[] | null = null;
+
   constructor(private state: StateService) {}
 
   ngOnInit(): void {
-    this.state.modeState$.subscribe((state) => (this.isCheckMode = state));
+    this.state.modeState$.subscribe((state) => {
+      this.isCheckMode = state;
+
+      if (!state) {
+        return;
+      }
+
+      this.currentState!.forEach((thing) => {
+        if (thing.correct === thing.selectedTo) {
+          this.counter++;
+        }
+      });
+    });
+    this.state.state$.subscribe((state) => (this.currentState = state));
   }
 }
